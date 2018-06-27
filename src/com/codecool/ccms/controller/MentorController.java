4fab732299e1f34475bf1248279a006e4cd59639
Mirosława Bryja <mira.bryja@gmail.com>
2018-06-27 13:35:25 +0200

    Add constructor

--- src/com/codecool/ccms/controller/MentorController.java
+++ src/com/codecool/ccms/controller/MentorController.java
@@ -5,4 +5,19 @@ import com.codecool.ccms.views.*;
 
 import java.util.*;
 
-public class MentorController {}
+public class MentorController {
+
+    private Mentor mentor;
+    private MentorView view;
+
+    private List<Assigment> templateAssigments;
+    private List<Assigment> assigmentsFromStudents;
+
+    public MentorController(Mentor mentor) {
+        this.mentor = mentor;
+        this.view = new MentorView();
+        this.templateAssigments = new ArrayList<>();
+        this.assigmentsFromStudents = new ArrayList<>();
+    }
+
+}
