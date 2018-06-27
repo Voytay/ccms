cefa53200d411631f59fde670667913a8af4321b
Mirosława Bryja <mira.bryja@gmail.com>
2018-06-27 13:04:22 +0200

    Create new model with getters and setters

---
+++ src/com/codecool/ccsm/models/Assigment.java
@@ -0,0 +1,41 @@
+package com.codecool.ccsm.models;
+
+public class Assigment{
+    private String URLadress;
+    private String studentEmail;
+    private Boolean submitted;
+    private String grade;
+
+    public Assigment(String URLadress) {
+        this.URLadress = URLadress;
+        this.studentEmail = null;
+        this.submitted = null;
+    }
+
+    public String getURLadress() {
+        return this.URLadress;
+    }
+
+    public void setURLadress(String URLadress) {
+        this.URLadress = URLadress;
+    }
+
+    public String getStudentEmail() {
+        return this.studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public Boolean getSubmitted() {
+        return this.submitted;
+    }
+
+    public void setSubmitted(Boolean submitted) {
+        this.submitted = submitted;
+    }
+
+
+    
+}
