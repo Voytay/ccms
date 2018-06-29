921358c7908e675d4bbe36f872e8e01c5226efb4
Mirosława Bryja <mira.bryja@gmail.com>
2018-06-29 07:14:08 +0200

    Implement getting students list from data base

---
+++ src/com/codecool/ccms/Parser.java
@@ -0,0 +1,32 @@
+package com.codecool.ccms;
+
+import com.codecool.ccms.model.*;
+
+import java.util.*;
+
+public class Parser {
+
+    private DataBase base;
+    private final namePosition = 0;
+    private final emailPosition = 1;
+    private final passwordPosition = 2;
+    private final statusPosition = 3;
+    private final activatedPosition = 4;
+
+    public DataBase() {
+        this.base = new DataBase();
+    }
+
+    public List<Student> getStudentsList() {
+        List<String> data = base.readDataFromFile("ccmsdata.csv");
+        for (String line: base) {
+            String arr[] = line.split(",");
+            if (arr[statusPosition] == Status.STAT_0.getStatus() && arr[activatedPosition] == 0) {
+                data.add(new Student(arr[namePosition], arr[emailPosition], false));
+            } else if (arr[statusPosition] == Status.STAT_0.getStatus() && arr[activatedPosition] == 1) {
+                data.add(new Student(arr[namePosition], arr[emailPosition], true));
+            }
+        }
+        return data;
+    }
+}
